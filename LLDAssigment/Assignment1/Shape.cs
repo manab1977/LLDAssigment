54d3a29a13b442f3187ed7fc3075541205d64425
Manabendra <md250572@ncr.com>
2023-05-24 17:33:54 +0530

    Initial commit

---
+++ LLDAssigment/Assignment1/Shape.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLDAssigment.Assignment1
+{
+    public class Shape
+    {
+        public double height;
+        public double width;
+
+        public Shape(double h, double w)
+        {
+            height= h;
+            width= w;
+        }
+        public virtual double ComputeArea()
+        {
+            return 0;
+        }
+
+        public void PrintArea(Shape shape)
+        {
+            Console.WriteLine("Area is " + shape.ComputeArea());
+        }
+    }
+
+    public class Triangle: Shape
+    {
+        public Triangle(double h, double w) : base(h, w)
+        {
+        }
+
+        public override double ComputeArea()
+        {
+            return (height * width) / 2;
+        }
+    }
+
+    public class Rectangle: Shape
+    {
+        public Rectangle(double h, double w): base(h, w)
+        {
+        }
+        public override double ComputeArea()
+        {
+            return (height * width);
+        }
+    }
+}
---
+++ LLDAssigment/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LLDAssigment.Assignment1;
+
+Console.WriteLine("Hello, World!");
+TestShape();
+
+void TestShape()
+{
+    Shape shape;
+    Console.WriteLine("Enter shape: triangle(t) or rectangle(r) ");
+    string? input = Console.ReadLine();
+    Console.WriteLine("Enter height");
+    double h = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter width");
+    double w = Convert.ToDouble(Console.ReadLine());
+
+    if (input == "t")
+    {
+        shape = new Triangle(h, w);
+    }
+    else
+    {
+        shape = new Rectangle(h, w);
+    }
+
+    shape.PrintArea(shape);
+}
